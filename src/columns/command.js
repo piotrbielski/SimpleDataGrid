c6f12e8776b82c0a9cc4f06f4f84b25563df0f2f
Piotr Bielski <bpiotrrr@gmail.com>
2019-10-05 19:05:23 +0200

    fixed command.js tracking (git)

---
+++ src/columns/command.js
@@ -0,0 +1,8 @@
+export default class Command {
+    constructor(buttonText, objectField, isHyperlink, afterExecuteCallback) {
+        this.buttonText = buttonText;
+        this.objectField = objectField;
+        this.isHyperlink = isHyperlink;
+        this.afterExecuteCallback = afterExecuteCallback;
+    }
+}
