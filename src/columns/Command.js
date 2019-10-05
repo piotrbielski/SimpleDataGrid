1e325bd17b84dbbe2a8131e0365f643852fcfe1b
Piotr Bielski <bpiotrrr@gmail.com>
2019-10-05 19:03:41 +0200

    rm test

--- src/columns/Command.js
+++
@@ -1,8 +0,0 @@
-export default class Command {
-    constructor(buttonText, objectField, isHyperlink, afterExecuteCallback) {
-        this.buttonText = buttonText;
-        this.objectField = objectField;
-        this.isHyperlink = isHyperlink;
-        this.afterExecuteCallback = afterExecuteCallback;
-    }
-}
